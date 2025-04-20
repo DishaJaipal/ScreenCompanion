819581f24cb8d92d3744f0be3df893899f2188cb
DishaJaipal <137790146+DishaJaipal@users.noreply.github.com>
2025-04-20 22:49:33 +0530

    Update voice_input.py

---
+++ chatbot/voice_input.py
@@ -0,0 +1,11 @@
+import speech_recognition as sr
+
+def listen_to_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+    try:
+        return r.recognize_google(audio)
+    except sr.UnknownValueError:
+        return "Sorry, I didn't catch that."
