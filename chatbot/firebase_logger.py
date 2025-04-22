9bc201b0af4a43322583b3a990d9ccc07a7ba35a
SindhushreeNH <sindhuharish311@gmail.com>
2025-04-22 14:58:34 +0530

    Update firebase_logger.py

--- chatbot/firebase_logger.py
+++ chatbot/firebase_logger.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("chatbot/serviceAccountKey.json")
+cred = credentials.Certificate("serviceAccountKey.json")  # New (correct path)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
